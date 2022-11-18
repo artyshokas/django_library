57efd5cdfc54fb9b8ba39ecb62c7e32b098ca8da
artyshokas <artiom.harchenko@gmail.com>
2022-11-18 14:49:19 +0200

    user profile, photo upload

--- ptu5_library/user_profile/models.py
+++ ptu5_library/user_profile/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        get_user_model(),
+        verbose_name="user",
+        on_delete=models.CASCADE,
+        related_name="profile",
+    )
+
+    photo = models.ImageField("photo", upload_to='user_profile/photo', null=True, blank=True, )
+
+
+def __str__(self):
+    return f"{self.user} profile"
