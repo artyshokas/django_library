2040e4b1a8dd1479ca81f1af5847f66928b1fc80
artyshokas <artiom.harchenko@gmail.com>
2022-11-23 14:36:04 +0200

    language select. base, settings, urls.

--- ptu5_library/ptu5_library/settings.py
+++ ptu5_library/ptu5_library/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -107,8 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-# LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'lt'
+LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'lt'
 
 LANGUAGES = [
     (LANGUAGE_CODE, _('US English')),
